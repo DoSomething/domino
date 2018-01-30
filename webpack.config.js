fd98d933e7a8b8ea7cbbaf69bb61e7606671c433
Shae Smith <sbsmith86@gmail.com>
2018-01-30 13:48:45 -0500

    add JS to index

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
 var webpack = require('webpack');
 var configure = require('@dosomething/webpack-config');
+const path = require('path');
 
 module.exports = configure({
     entry: {
