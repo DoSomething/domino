2fdf2206d0e80607475082347a9d571425e9ed8e
Chloe Lee <chloealessandra@gmail.com>
2018-02-13 16:05:55 -0500

    edits to show repeat standings rank

--- js/components/Table/index.js
+++ js/components/Table/index.js
@@ -8,21 +8,19 @@ class Table extends React.Component {
   constructor() {
     super();
 
-    this.addRankToUsers = this.addRankToUsers.bind(this);
+    this.addRepeatedStandingsRankToUsers = this.addRepeatedStandingsRankToUsers.bind(this);
   }
 
-  addRankToUsers(data) {
+  addRepeatedStandingsRankToUsers(data) {
     let increment = 1;
     let rank = 1;
-    const lastElement = data[data.length - 1];
 
     data.forEach(function(element, index) {
       // Don't perform this logic on the first element
       if (index > 0) {
-        // If the last row quantity equals this rows quantity, just increment.
-        if (element['quantity'] === lastElement['quantity']) {
-          increment++;
-        // Otherwise apply the increment to the rank and reset it back to 1.
+        // Apply "repeated standings" rank and reset it back to 1.
+        if (element['quantity'] === data[index - 1]['quantity']) {
+          rank = data[index - 1]['rank']
         } else {
           rank += increment;
           increment = 1;
@@ -38,7 +36,7 @@ class Table extends React.Component {
 
   render() {
     const heading = this.props.headings.map((title, index) => <th key={index} className="table__cell"><h3 className="heading -delta">{title}</h3></th>);
-    const users = this.addRankToUsers(this.props.data);
+    const users = this.addRepeatedStandingsRankToUsers(this.props.data);
 
     const rows = users.map((content, index) => {
       return <Row key={index} data={content} />;
