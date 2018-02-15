b768fa45c78142596a5cda80c5e86a440f6356db
katiecrane <kcrane@dosomething.org>
2018-02-15 14:13:38 -0500

    Add college logos in own column

--- js/components/Table/Row.js
+++ js/components/Table/Row.js
@@ -21,11 +21,17 @@ class Row extends React.Component {
     return row;
   }
 
+  getImage(data) {
+    return 'images/college_logos/' + data.northstar_id +'.png';
+  }
+
   render() {
     const content = this.createRow(this.props.data);
+    const image = this.getImage(this.props.data);
 
     return (
       <tr className="table__row">
+        <td className="table__cell"><img src={image}/></td>
         {content.map((cell, index) => <td className="table__cell" key={index}>{cell.title}</td>)}
       </tr>
     );
