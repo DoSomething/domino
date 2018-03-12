42cb71e36281cdab12da033c7d11adc51df5eeba
katiecrane <kcrane@dosomething.org>
2018-03-12 15:01:25 -0400

    Add college name structure to be filled with data

--- js/components/Table/Row.js
+++ js/components/Table/Row.js
@@ -10,15 +10,24 @@ class Row extends React.Component {
     return 'images/college_logos/' + data.northstar_id +'.png';
   }
 
+  getCollegeName(data) {
+    const collegeNames = {
+      // 'northstar_id_here': 'Not Real University',
+    };
+
+    return collegeNames[data.northstar_id];
+  }
+
   render() {
     const image = this.getImage(this.props.data);
     const rank = this.props.data.rank.toString().concat('.');
     const firstName = this.props.data.user.data.first_name;
+    const collegeName = this.getCollegeName(this.props.data);
     const quantity = this.props.data.quantity;
 
     return (
       <tr className="table__row">
-        <td className="table__cell">{rank}<img src={image}/>{firstName}</td>
+        <td className="table__cell">{rank}<img src={image}/>{firstName} {collegeName}</td>
         <td className="table__cell">{quantity || 0}</td>
       </tr>
     );
